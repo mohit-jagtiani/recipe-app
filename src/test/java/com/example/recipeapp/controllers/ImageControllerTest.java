ef89fe054f599a8ef51700b2a5b640cc089be12a
huntum <mohit.jagtiani@sytac.io>
2020-05-25 01:34:32 +0200

    fixed failing test at global level

--- src/test/java/com/example/recipeapp/controllers/ImageControllerTest.java
+++ src/test/java/com/example/recipeapp/controllers/ImageControllerTest.java
@@ -36,7 +36,9 @@ public class ImageControllerTest {
         MockitoAnnotations.initMocks(this);
 
         controller = new ImageController(imageService, recipeService);
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setControllerAdvice(new ControllerExceptionHandler())
+                .build();
     }
 
     @Test
@@ -100,10 +102,10 @@ public class ImageControllerTest {
         assertEquals(s.getBytes().length, reponseBytes.length);
     }
 
-  /*  @Test
+   @Test
     public void testGetImageNumberFormatException() throws Exception {
         mockMvc.perform(get("/recipe/asdf/recipeimage"))
                 .andExpect(status().isBadRequest())
                 .andExpect(view().name("400error"));
-    } */
+    }
 }
